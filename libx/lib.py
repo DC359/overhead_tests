6d89e2a502f858d6506b4f612f017e05a1f3fcec
DC359 <dhruv.choudhary@nutanix.com>
2026-09-17 14:36:09 +0530

    security: remove hardcoded VM password from lib.py

--- libx/lib.py
+++ libx/lib.py
@@ -1,6 +1,6 @@
 import subprocess
+from util.credentials import get_vm_password
 
-VM_PASSWORD = "nutanix/4u"
 SSH_OPTS = "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR"
 
 def shell_run(cmd):
@@ -11,21 +11,24 @@ def shell_run_non_blocking(cmd):
 
 def run_remote_cmd(host_ip, user, cmd, use_password=False):
     if use_password:
-        full_cmd = "sshpass -p '%s' ssh %s %s@%s '%s'" % (VM_PASSWORD, SSH_OPTS, user, host_ip, cmd.replace("'", "'\\''"))
+        password = get_vm_password()
+        full_cmd = "sshpass -p '%s' ssh %s %s@%s '%s'" % (password, SSH_OPTS, user, host_ip, cmd.replace("'", "'\\''"))
     else:
         full_cmd = "ssh %s %s@%s '%s'" % (SSH_OPTS, user, host_ip, cmd.replace("'", "'\\''"))
     return shell_run(full_cmd)
 
 def run_remote_cmd_non_blocking(host_ip, user, cmd, use_password=False):
     if use_password:
-        full_cmd = "sshpass -p '%s' ssh %s %s@%s '%s'" % (VM_PASSWORD, SSH_OPTS, user, host_ip, cmd.replace("'", "'\\''"))
+        password = get_vm_password()
+        full_cmd = "sshpass -p '%s' ssh %s %s@%s '%s'" % (password, SSH_OPTS, user, host_ip, cmd.replace("'", "'\\''"))
     else:
         full_cmd = "ssh %s %s@%s '%s'" % (SSH_OPTS, user, host_ip, cmd.replace("'", "'\\''"))
     return shell_run_non_blocking(full_cmd)
 
 def scp_add_file(srcpath, destHost, user, destpath, use_password=False):
     if use_password:
-        full_cmd = "sshpass -p '%s' scp %s %s %s@%s:/%s" % (VM_PASSWORD, SSH_OPTS, srcpath, user, destHost, destpath)
+        password = get_vm_password()
+        full_cmd = "sshpass -p '%s' scp %s %s %s@%s:/%s" % (password, SSH_OPTS, srcpath, user, destHost, destpath)
     else:
         full_cmd = "scp %s %s %s@%s:/%s" % (SSH_OPTS, srcpath, user, destHost, destpath)
     return shell_run(full_cmd)
